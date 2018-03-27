99ce27cdc755c93233999d61639498758208f402
kouwasi <kouwasi@gmail.com>
2018-03-28 02:29:38 +0900

    add dependencies

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule SteamEx.Mixfile do
   def project do
     [
       app: :steam_ex,
-      version: "0.1.0",
+      version: "0.1.0-alpha",
       elixir: "~> 1.5",
       start_permanent: Mix.env == :prod,
       deps: deps()
@@ -21,8 +21,8 @@ defmodule SteamEx.Mixfile do
   # Run "mix help deps" to learn about dependencies.
   defp deps do
     [
-      # {:dep_from_hexpm, "~> 0.3.0"},
-      # {:dep_from_git, git: "https://github.com/elixir-lang/my_dep.git", tag: "0.1.0"},
+      {:httpoison, "~> 1.0"},
+      {:poison, "~> 1.5"}
     ]
   end
 end
