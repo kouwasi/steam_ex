b773340a98ab5903ec1c3344b075b609614e0fcf
kouwasi <kouwasi@gmail.com>
2018-03-28 22:29:20 +0900

    write publish configurations

--- mix.exs
+++ mix.exs
@@ -7,7 +7,11 @@ defmodule SteamEx.Mixfile do
       version: "0.2.0-alpha",
       elixir: "~> 1.5",
       start_permanent: Mix.env == :prod,
-      deps: deps()
+      description: description(),
+      package: package(),
+      deps: deps(),
+      name: "SteamEx",
+      source_url: "https://github.com/kouwasi/steam_ex"
     ]
   end
 
@@ -24,6 +28,22 @@ defmodule SteamEx.Mixfile do
       {:httpoison, "~> 1.0"},
       {:ex_doc, "~> 0.18.3", only: :dev, runtime: false},
       {:earmark, "~> 1.2", only: :dev, runtime: false}
+    ]
+  end
+
+  defp description() do
+    "Elixir wrapper for the Steam Web API"
+  end
+
+  defp package() do
+    [
+      # This option is only needed when you don't want to use the OTP application name
+      name: "steam_ex",
+      # These are the default files included in the package
+      files: ["lib", "mix.exs", "README*", "LICENSE*"],
+      maintainers: ["kouwasi"],
+      licenses: ["MIT License"],
+      links: %{"GitHub" => "https://github.com/kouwasi/steam_ex"}
     ]
   end
 end
